d593cb72bc6b65fe5c8db40ce1a32676a46c14c4
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-08-04 10:33:56 +0000

    [Add]4-6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  # devise利用の機能（ユーザ登録、ログイン認証など）が使われる前に
+  # 下に記述したconfigure_permitted_parametersメソッドが実行される
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # 呼び出された他のコントローラからも参照することができる
+  protected
+
+  # ユーザー登録(sign_up)の際に、ユーザー名(name)のデータ操作を許可する
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
