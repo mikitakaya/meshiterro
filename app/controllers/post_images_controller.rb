d07b9dad483e1a2417d0f82c518b193eb9603d7d
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-08-05 09:42:10 +0000

    [Finish]4-10

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,15 +1,17 @@
 class PostImagesController < ApplicationController
  def new
  # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを作成する
-  @post_image = Post.new
+  @post_image = PostImage.new
  end
 
  # 投稿データの保存
  def create
+  # フォームに入力されたデータを、@post_imageに格納する
   @post_image = PostImage.new(post_image_params)
+  # PostImageモデルに紐づいたuser_idの値を操作し、ログイン中のユーザーidを取得する
   @post_image.user_id = current_user.id
   @post_image.save
-  # indexにリダイレクト
+  # index（投稿一覧画面）にリダイレクト
   redirect_to post_images_path
  end
 
@@ -25,6 +27,7 @@ class PostImagesController < ApplicationController
  # 投稿データのストロングパラメータ
  private
 
+# フォームで入力されたデータが、投稿データとして許可されているパラメータかチェック
  def post_image_params
   params.require(:post_image).permit(:shop_name, :image, :caption)
  end
