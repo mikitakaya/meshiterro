5f7bfbba888bb9cd1b72138edf730a9bf21fb05b
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-08-05 14:07:57 +0000

    [Start]4-14

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,12 @@ class User < ApplicationRecord
   # User モデルに PostImage モデルを関連付ける(1:N)
   # 1:Nの1側(user)が削除されたとき、N側(post_images複数形)を全て削除する
   has_many :post_images, dependent: :destroy
+
+  def get_profile_image
+   unless profile_image.attached?
+    file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+   end
+   profile_image.variant(resize_to_limit: [100, 100]).processed
+  end
 end
