aa27aa6f1deb2b5eb7306c1061fb3d89884f13c7
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-08-05 08:28:34 +0000

    [Add]4-9

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,3 +1,5 @@
 class PostImage < ApplicationRecord
  has_one_attached :image
+# ユーザー（1:Nの1だから単数形）に属する
+ belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # User モデルに PostImage モデルを関連付ける(1:N)
+  # 1:Nの1側(user)が削除されたとき、N側(post_images複数形)を全て削除する
   has_many :post_images, dependent: :destroy
 end
