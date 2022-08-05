830f4f687bfb3269cddbd97156fe6dafd4db6448
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-08-05 09:20:10 +0000

    [Add]4-10

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,12 +1,32 @@
 class PostImagesController < ApplicationController
-  def new
-  # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを作成する
-   @post_image = Post.new
-  end
+ def new
+ # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを作成する
+  @post_image = Post.new
+ end
 
-  def index
-  end
+ # 投稿データの保存
+ def create
+  @post_image = PostImage.new(post_image_params)
+  @post_image.user_id = current_user.id
+  @post_image.save
+  # indexにリダイレクト
+  redirect_to post_images_path
+ end
 
-  def show
-  end
+ def index
+ end
+
+ def show
+ end
+
+ def destroy
+ end
+
+ # 投稿データのストロングパラメータ
+ private
+
+ def post_image_params
+  params.require(:post_image).permit(:shop_name, :image, :caption)
+ end
+
 end
